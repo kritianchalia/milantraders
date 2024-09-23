2429a7ea76d4ac742f1db49b9850d060f4ca769c
kritianchalia <kritianchalia2@gmail.com>
2024-09-23 22:35:50 +0530

    adding files

---
+++ homepage.js
@@ -0,0 +1,51 @@
+//mail copy
+
+let copy=function(event){
+  const textToCopy = "milantraders.in@gmail.com";
+  
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        console.log("Text copied successfully: " + textToCopy);
+      })
+      .catch((error) => {
+        console.error("Unable to copy text: ", error);
+      });
+  } else {
+    console.error("Clipboard API not supported");
+    console.log("hi");
+  }
+}
+
+const copyButton = document.querySelectorAll(".copymail");
+for(var i = 0; i < copyButton.length; i++)
+{
+  copyButton[i].addEventListener('click', copy, false);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+  })
+})
+
+//mail copy end
+
+
+
+
+const menu = document.querySelector('#menu');
+const hidemenu = document.querySelector('#hidemenu');
+const sidebar = document.querySelector('.sidebar');
+menu.addEventListener("click" , ()=>{
+    sidebar.style.display = "flex"
+})
+hidemenu.addEventListener("click" , ()=>{
+    sidebar.style.display = "none"
+})
+
+
+
+
+
